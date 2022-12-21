945fe0d101947e3edf98870ee582b247f22e2f1a
lanjewarakash <lanjewarakash72@gmail.com>
2022-12-21 14:00:04 +0530

    Added: Sorting Int Float String Program

--- src/com/blz/GenericMain.java
+++ src/com/blz/GenericMain.java
@@ -1,10 +1,41 @@
 package com.blz;
 
-public class GenericMain {
+import java.util.Scanner;
+
+public class GenericMain<T> {
     public static void main(String[] args) {
         System.out.println("Welcome to Generic Programs");
 
-        MaximumNum max = new MaximumNum<>("Akash",  "Ram",  "Lanjewar");
-        max.printMaxStrings();
+        System.out.println(" Press 1 to Sort Integers  \n Press 2 to Sort Floats \n Press 3 to Sort Strings");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                findingMaxInteger();
+                break;
+            case 2:
+                findingMaxFloat();
+                break;
+            case 3:
+                findingMaxString();
+        }
+
+    }
+
+    static MaximumNum<Integer> object = new MaximumNum<>();
+
+    public static void findingMaxInteger() {
+        System.out.println("\ninteger Sorted in ascending order.....................");
+        object.compare(40, 30, 10, 20);
+    }
+
+    public static void findingMaxFloat() {
+        System.out.println("\nFloat Sorted in ascending order.....................");
+        object.compare(46.86f, 87.87f, 34.65f, 42.12f);
+    }
+
+    public static void findingMaxString() {
+        System.out.println("\nSorted in ascending order.....................");
+        object.compare("Akash", "Ram", "Lanjewar", "Nagpur");
     }
 }
