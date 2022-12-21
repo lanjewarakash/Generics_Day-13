a15db74fd18db82c2e16579887d66e4b62183299
lanjewarakash <lanjewarakash72@gmail.com>
2022-12-21 12:36:18 +0530

    Added: Find Maximum Integers

--- src/com/blz/GenericMain.java
+++ src/com/blz/GenericMain.java
@@ -3,5 +3,8 @@ package com.blz;
 public class GenericMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Generic Programs");
+
+        MaximumNum max = new MaximumNum<>(10, 20, 30);
+        max.printMax();
     }
 }
---
+++ src/com/blz/MaximumNum.java
@@ -0,0 +1,27 @@
+package com.blz;
+
+public class MaximumNum<T extends Comparable> {
+    T x;
+    T y;
+    T z;
+
+    public MaximumNum(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public void printMax() {
+        com.blz.MaximumNum.printMax(x, y, z);
+    }
+
+    private static <T extends Comparable> void printMax(T x, T y, T z) {
+        if (x.compareTo(y) > 0 && x.compareTo(z) > 0) {
+            System.out.println(x);
+        } else if (y.compareTo(x) > 0 && y.compareTo(z) > 0) {
+            System.out.println(y);
+        } else {
+            System.out.println(z);
+        }
+    }
+}
